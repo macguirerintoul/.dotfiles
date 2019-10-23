176d6c3007132ce810efa7eaadf7a6a909d1aca7
Macguire Rintoul <18323154+macguirerintoul@users.noreply.github.com>
2019-10-22 21:51:25 -0700

    add spotify

--- install/install-2.sh
+++ install/install-2.sh
@@ -6,5 +6,6 @@ brew install antigen
 # install nvm, node.js version manager
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
 
-# install iterm2
+# install apps via cask
 brew cask install iterm2
+brew cask install spotify
