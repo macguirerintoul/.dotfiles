dacf3dd46d1408bbd51b6aa3ad031f3270034f82
Macguire Rintoul <macguire.rintoul@gmail.com>
2019-10-25 15:55:53 -0700

    add atom and package-sync

--- install/install-2.sh
+++ install/install-2.sh
@@ -9,3 +9,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
 # install apps via cask
 brew cask install iterm2
 brew cask install spotify
+brew cask install atom
+
+# install atom package to sync packages
+apm install package-sync
