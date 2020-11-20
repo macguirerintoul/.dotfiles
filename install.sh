763964965d56000ffcb3b1060f67648dfa3eb772
Macguire Rintoul <macguire.rintoul@protonmail.ch>
2020-11-20 04:38:12 +0000

    fix the whole dang install script

--- install.sh
+++ install.sh
@@ -1,8 +1,40 @@
 DOTDIR=~/.dotfiles
+OS=null
 
-# Clone my dotfiles repo
-if find ~/.dotfiles -maxdepth 0 -empty | read v; then
+# Clone my dotfiles repo if it doesn't already exist
+if [ -d "~/.dotfiles" ]; then
 	git clone https://github.com/macguirerintoul/.dotfiles.git ~/.dotfiles
+fi
+
+# Check what OS we're on for later
+if command -v apt &> /dev/null; then
+	# We're on Linux 😎
+	OS=Linux
+elif [[ `uname` == "Darwin" ]]; then
+	OS=macOS
+else
+	# Who knows?
+	echo 'Unknown OS!'
+fi
+
+# Get stow if we don't have it
+if command -v stow &> /dev/null; then
+	if $OS == 'Linux'; then
+		apt install stow
+	elif $OS == 'macOS'; then
+		# Note sure if macOS comes with stow or not, but let's get it anyways.
+		# Source .zshrc if it exists;
+		# We can't know if brew is installed until we run the 'eval' thing for brew that you have to put in .zshrc
+		if [ -f "~/.zshrc" ]; then
+			source ~/.zshrc
+		fi
+		# If brew isn't installed, get it
+		if ! type brew > /dev/null; then
+			/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+		fi
+		# Then, get stow!
+		brew install stow
+	fi
 fi
 
 # Run stow on every directory in ~/.dotfiles
@@ -13,40 +45,34 @@ for dir in */; do
 	stow "$dir" -t ~ -v
 done
 
-# Install vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# Now, .zshrc is where it's supposed to be and we can source it (on Linux, we haven't yet)
+source ~/.zshrc
 
-# Run OS-specific scripts
-if command -v apt &> /dev/null; then
-	# We're on Linux
-	source $DOTDIR/linux/linux.sh
-	cd $DOTDIR/linux 
-elif [[ `uname` == "Darwin" ]]; then
-	# We're on macOS
-	source $DOTDIR/macos.sh
-	cd $DOTDIR/macos
-else
-	# Who knows?
-	echo 'Unknown OS!'
-fi
-brew bundle -v
-
-# If Homebrew isn't installed, install it
+# If brew isn't installed, get it (on Linux, we haven't yet)
 if ! type brew > /dev/null; then
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
+# Run OS-specific scripts
+# and go to the directory where each OS' Brewfile is
+if $OS == 'Linux'; then
+	source $DOTDIR/linux/linux.sh
+	cd $DOTDIR/linux 
+elif $OS == macOS; then
+	source $DOTDIR/macos.sh
+	cd $DOTDIR/macos
+fi
+brew bundle -v # Then, install from the Brewfile
+
 # Install nvm if not installed
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 if ! type nvm > /dev/null; then
 	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.0/install.sh | bash
 fi
 
-# Source ~/.zshrc so brew/nvm/... can be used
-source ~/.zshrc
-
 # Install Node.js LTS via nvm
 nvm install --lts
+
+# Install vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
